79a5b8913619bffa54bcadfe3bb89d7096d0be10
HomieRaph <raphaelpaulus6@gmail.com>
2024-12-01 02:18:11 +0700

    a

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory('assets', filename)
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
