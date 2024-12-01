f9502863592a668cec85abdaf123beadac9e84fb
HomieRaph <raphaelpaulus6@gmail.com>
2024-12-01 13:11:56 +0700

    a

--- app.py
+++ app.py
@@ -14,5 +14,36 @@ def serve_static(filename):
 def index():
     return render_template('index.html')
 
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 if __name__ == "__main__":
     app.run()
